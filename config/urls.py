63099df8863ef77630b23f7a8d3d7614ffe408c5
Tiago Somera <tiago.sxsilveira@gmail.com>
2026-05-05 18:16:42 -0300

    Ajuste: Separar Professores e Disciplinas em .html e .css

--- config/urls.py
+++ config/urls.py
@@ -16,7 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from core import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('disciplinas/', views.disciplinas, name='disciplinas'),
+    path('professores/', views.professores, name='professores'),
 ]
--- core/views.py
+++ core/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
 # Create your views here.
+def disciplinas(request):
+    return render(request, 'core/disciplinas.html')
+
+def professores(request):
+    return render(request, 'core/professores.html')
